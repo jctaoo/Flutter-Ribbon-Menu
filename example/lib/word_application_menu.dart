cbd678874684465640f23438ba57514ab57f1728
timmer98 <38386858+timmer98@users.noreply.github.com>
2022-04-08 21:11:18 +0200

    Extended example application menu with buttons

--- example/lib/word_application_menu.dart
+++ example/lib/word_application_menu.dart
@@ -10,29 +10,84 @@ class WordApplicationMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return RibbonApplicationMenu(
-        buttonLabel: "File",
-        closeAction: () {},
-        menuItems: [
-          RibbonApplicationMenuDropdownButton(
+      buttonLabel: "File",
+      closeAction: () {},
+      menuItems: [
+        RibbonApplicationMenuDropdownButton(
+          onPressed: () {},
+          icon: const Icon(Icons.window),
+          label: "Open new window",
+          items: [
+            RibbonApplicationMenuDropdownItem(
               onPressed: () {},
-              icon: const Icon(Icons.window),
-              label: "Open new window",
-              items: [
-                RibbonApplicationMenuDropdownItem(
-                  onPressed: () {},
-                  icon: const Icon(Icons.window_outlined),
-                  label: "Open new Terminal",
-                ),
-                RibbonApplicationMenuDropdownItem(
-                    onPressed: () {},
-                    icon: const Icon(Icons.window_outlined),
-                    label: "Open new Terminal in Process",),
-              ],),
-          RibbonApplicationMenuButton(
-            icon: const Icon(Icons.add_a_photo_sharp),
-            label: "Change folder",
-            onPressed: () {},
-          )
-        ],);
+              icon: const Icon(Icons.window_outlined),
+              label: "Open new Terminal",
+            ),
+            RibbonApplicationMenuDropdownItem(
+              onPressed: () {},
+              icon: const Icon(Icons.window_outlined),
+              label: "Open new Terminal in Process",
+            ),
+          ],
+        ),
+        RibbonApplicationMenuDropdownButton(
+          onPressed: () {},
+          icon: const Icon(Icons.code),
+          label: "Open Windows PowerShell",
+          items: const [
+            RibbonApplicationMenuDropdownItem(
+              icon: Icon(Icons.code),
+              label: "Open Windows PowerShell",
+            ),
+            RibbonApplicationMenuDropdownItem(
+              icon: Icon(Icons.admin_panel_settings),
+              label: "Open Windows PowerShell as administrator",
+            )
+          ],
+        ),
+        const Divider(
+          height: 8,
+          indent: 52,
+          color: Colors.grey,
+        ),
+        RibbonApplicationMenuButton(
+          icon: const Icon(Icons.list_alt),
+          label: "Change folder and search options",
+          onPressed: () {},
+        ),
+        RibbonApplicationMenuDropdownButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.help,
+            color: Colors.blue,
+          ),
+          label: "Help",
+          items: const [
+            RibbonApplicationMenuDropdownItem(
+              icon: Icon(
+                Icons.help,
+                color: Colors.blue,
+              ),
+              label: "Help",
+            ),
+            RibbonApplicationMenuDropdownItem(
+              icon: Icon(
+                Icons.info,
+                color: Colors.blue,
+              ),
+              label: "About windows",
+            )
+          ],
+        ),
+        RibbonApplicationMenuButton(
+          onPressed: () {},
+          label: "Exit",
+          icon: const Icon(
+            Icons.cancel,
+            color: Colors.red,
+          ),
+        )
+      ],
+    );
   }
 }
